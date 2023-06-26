4d9b1136131e703a9e890d99c027f95c2c2beb7e
Engefu <brilliantengefu@gmail.com>
2023-06-26 05:31:17 -0700

    Update 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         idx = 0
         for i in my_list:
-            print(i, end=' ')
-            idx += 1
+            try:
+                print("{:d}".format(i), end=' ')
+                idx += 1
+            except TypeError:
+                pass
             if idx == x:
                 break
         print()
